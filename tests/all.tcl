99210a5510abb0321b929bc60d4f17d0eac86310
hobbs <>
2002-03-29 05:06:52 +0000

    added message about starting pwd

--- tests/all.tcl
+++ tests/all.tcl
@@ -20,17 +20,18 @@ set ::tcltest::testsDirectory [file dir [info script]]
 # We need to ensure that the testsDirectory is absolute
 ::tcltest::normalizePath ::tcltest::testsDirectory
 
-puts stdout "Tests running in interp:  [info nameofexecutable]"
+puts stdout "Tests running in interp:       [info nameofexecutable]"
+puts stdout "Tests running with pwd:        [pwd]"
 puts stdout "Tests running in working dir:  $::tcltest::testsDirectory"
 if {[llength $::tcltest::skip] > 0} {
-    puts stdout "Skipping tests that match:  $::tcltest::skip"
+    puts stdout "Skipping tests that match:            $::tcltest::skip"
 }
 if {[llength $::tcltest::match] > 0} {
-    puts stdout "Only running tests that match:  $::tcltest::match"
+    puts stdout "Only running tests that match:        $::tcltest::match"
 }
 
 if {[llength $::tcltest::skipFiles] > 0} {
-    puts stdout "Skipping test files that match:  $::tcltest::skipFiles"
+    puts stdout "Skipping test files that match:       $::tcltest::skipFiles"
 }
 if {[llength $::tcltest::matchFiles] > 0} {
     puts stdout "Only sourcing test files that match:  $::tcltest::matchFiles"
