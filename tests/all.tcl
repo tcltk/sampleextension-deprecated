ace5160bee84d81e46bcb8658e5a66f9d5f8d8ea
hobbs <>
2002-07-11 23:54:52 +0000

    minor formatting improvements

--- tests/all.tcl
+++ tests/all.tcl
@@ -20,37 +20,39 @@ set ::tcltest::testsDirectory [file dir [info script]]
 # We need to ensure that the testsDirectory is absolute
 ::tcltest::normalizePath ::tcltest::testsDirectory
 
-puts stdout "Tests running in interp:       [info nameofexecutable]"
-puts stdout "Tests running with pwd:        [pwd]"
-puts stdout "Tests running in working dir:  $::tcltest::testsDirectory"
+set chan $::tcltest::outputChannel
+
+puts $chan "Tests running in interp:       [info nameofexecutable]"
+puts $chan "Tests running with pwd:        [pwd]"
+puts $chan "Tests running in working dir:  $::tcltest::testsDirectory"
 if {[llength $::tcltest::skip] > 0} {
-    puts stdout "Skipping tests that match:            $::tcltest::skip"
+    puts $chan "Skipping tests that match:            $::tcltest::skip"
 }
 if {[llength $::tcltest::match] > 0} {
-    puts stdout "Only running tests that match:        $::tcltest::match"
+    puts $chan "Only running tests that match:        $::tcltest::match"
 }
 
 if {[llength $::tcltest::skipFiles] > 0} {
-    puts stdout "Skipping test files that match:       $::tcltest::skipFiles"
+    puts $chan "Skipping test files that match:       $::tcltest::skipFiles"
 }
 if {[llength $::tcltest::matchFiles] > 0} {
-    puts stdout "Only sourcing test files that match:  $::tcltest::matchFiles"
+    puts $chan "Only sourcing test files that match:  $::tcltest::matchFiles"
 }
 
 set timeCmd {clock format [clock seconds]}
-puts stdout "Tests began at [eval $timeCmd]"
+puts $chan "Tests began at [eval $timeCmd]"
 
 # source each of the specified tests
 foreach file [lsort [::tcltest::getMatchingFiles]] {
     set tail [file tail $file]
-    puts stdout $tail
+    puts $chan $tail
     if {[catch {source $file} msg]} {
-	puts stdout $msg
+	puts $chan $msg
     }
 }
 
 # cleanup
-puts stdout "\nTests ended at [eval $timeCmd]"
+puts $chan "\nTests ended at [eval $timeCmd]"
 ::tcltest::cleanupTests 1
 return
 
